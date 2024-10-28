f8b19582c4e0b9e5945c2d83982774c54790b092
Chewbacca <higgimat000@warren.k12.in.us>
2024-10-28 13:59:18 -0400

    initial commit

--- src/StudentEnrollment.java
+++ src/StudentEnrollment.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class StudentEnrollment {
+public class StudentEnrollment {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println(student1);
+
+        Student student2 = new Student();
+        System.out.println(student2);
+
+        Student student3 = new Student("58956", "Michael", "McDonald", 72, "Music");
+        System.out.println(student3);
+
+        Student student4 = new Student("12345", "Barney", "Rubble", 31, "Geology");
+        System.out.println(student4);
+        student4.setMajor("Computer Science");
+        System.out.println(student4);
+
+
+    }
 }
